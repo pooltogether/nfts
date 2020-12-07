876e72b6b67293b54b20ff2cc7d17d57c0410afc
Charles Bergeron <chuck.bergeron@gmail.com>
2020-12-06 17:15:19 -0800

    * add cors support * fill in metadata * add icon

--- pages/api/ptloot/[id].js
+++ pages/api/ptloot/[id].js
@@ -1,8 +1,38 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import Cors from 'cors'
 
-export default (req, res) => {
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+async function handler(req, res) {
+  const {
+    query: { id },
+  } = req
+
+  await runMiddleware(req, res, cors)
+
   res.statusCode = 200
   res.json({
     name: 'PoolTogether LootBox',
+    description: 'PoolTogether players can win boxes full of loot to plunder. A LootBox can store ERC20s, ERC721s and ERC1155s',
+    image: 'https://nfts.pooltogether.com/icon-lootbox@2x.png',
+    external_url: `https://nfts.pooltogether.com/api/nfts/${id}`, 
   })
 }
+
+export default handler
