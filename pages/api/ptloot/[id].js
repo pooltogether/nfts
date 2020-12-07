be16092ee33c58bd3c440d4cb2b8662b918693c8
Charles Bergeron <chuck.bergeron@gmail.com>
2020-12-06 18:29:46 -0800

    * don't want next export

--- pages/api/ptloot/[id].js
+++ pages/api/ptloot/[id].js
@@ -31,7 +31,7 @@ async function handler(req, res) {
     name: 'PoolTogether LootBox',
     description: 'PoolTogether players can win boxes full of loot to plunder. A LootBox can store ERC20s, ERC721s and ERC1155s',
     image: 'https://nfts.pooltogether.com/icon-lootbox@2x.png',
-    external_url: `https://nfts.pooltogether.com/api/nfts/${id}`, 
+    external_url: `https://nfts.pooltogether.com/api/ptloot/${id}`, 
   })
 }
 
